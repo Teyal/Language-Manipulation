47c9244a013f580abb17272b52c05e4425540806
Teo <teogallarza@gmail.com>
2019-07-04 01:22:23 +0000

    added differences to afnd and some things else

--- languages/af/afnd.py
+++ languages/af/afnd.py
@@ -1,4 +1,4 @@
-class automato:
+class afnd:
     """# colocar entrada?"""
     def __init__(self, estados, inicial, finais, transicoes):
         self.estados = estados
@@ -9,18 +9,37 @@ class automato:
 
     def testar_entrada(self, entrada):
         estado = self.inicial
+        print(entrada)
         for i in entrada:
+            # print(i)
             estado = self._proximo_estado(estado, i)
+
         if estado in self.finais:
             print("Entrada válida")
             return True
         else:
+            # print(estado)
             print("Entrada inválida")
             return False
 
     def _proximo_estado(self, estado, entrada):
         if entrada in self.transicoes[estado]:
+            print("{}, {}".format(estado, entrada))
             return self.transicoes[estado][entrada]
         else:
             print("transição inválida, estado morto alcançado")
             return "q-1"
+
+    def fechamento_lambda(self, inicial):
+        lista = []
+        lista.append(inicial)
+        retorno = set()
+
+        while lista:
+            estado = lista.pop()
+            if estado not in retorno:
+                retorno.add(estado)
+                if '' in self.transicoes[estado]:
+                    lista.extend(self.transicoes[estado][''])
+
+        return retorno
--- languages/af/testsND.py
+++ languages/af/testsND.py
@@ -1,12 +1,11 @@
-from automato import automato
-af = automato(
+from afnd import afnd
+af = afnd(
     estados={'q0', 'q1', 'q2'},
     inicial='q0',    finais={'q1'},
     transicoes={
-        """nao sei multiplas trnaiscoes E do msm estado"""
-        'q0': {'': 'q1', '0': 'q3'},
+        'q0': {'0': 'q1', '': 'q2'},
         'q1': {'0': 'q1', '1': 'q2'},
         'q2': {'0': 'q2', '1': 'q1'}
     })
-print(af.transicoes)
-af.testar_entrada('1')
+# print(af.transicoes)
+af.testar_entrada("01")
